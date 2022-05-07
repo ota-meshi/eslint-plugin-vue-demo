6a57599a4e18746233ffef6050cc1844fc458e3b
yosuke ota <otameshiyo23@gmail.com>
2022-05-07 12:04:22 +0900

    fix for ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 
+if (typeof window !== "undefined" && typeof window.process === "undefined") {
+  window.process = {
+    env: {},
+    cwd: () => "",
+  } as any
+}
 createApp(App).mount("#app")
