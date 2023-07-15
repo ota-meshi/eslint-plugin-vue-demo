071e5f38ef13e1e4781acb1bbb9c254ef8509412
Yosuke Ota <otameshiyo23@gmail.com>
2023-07-15 12:24:04 +0000

    fix

--- shim/eslint/index.js
+++ shim/eslint/index.js
@@ -1,5 +1,5 @@
 /* eslint n/no-unsupported-features/es-syntax:0 -- ignore */
-// eslint-disable-next-line n/no-unpublished-import -- ignore
+// eslint-disable-next-line n/no-missing-import -- ignore
 import { Linter } from "../../node_modules/eslint/lib/linter/index"
 export { Linter }
 export default { Linter }
